2b96c95e2e6ba4f084ab07645afc1b9e5302295c
creeper4004 <40329851+creeper4004@users.noreply.github.com>
2020-02-09 23:22:15 -0600

    Add files via upload

--- atm.py
+++ atm.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from main import *
 
+#This is the main menu from the ATM system
 def atm(username, user_id):
     try:
         os.system("clear")
+        #printing the main menu
+        tprint("ATM")
         print("Welcome {} to the best ATM ever created!".format(username))
         print("""
             1 to deposit money
@@ -12,11 +15,12 @@ def atm(username, user_id):
             4 to deposit money to other client
             5 to quit or use CTRL+C
             """)
+        #asking for the operation to do
         option = int(input("Write your option: "))
         if option == 1:
             deposit_money(username, user_id)
         elif option == 2:
-            withdraw_money(user_id)
+            take_money_out(user_id)
         elif option == 3:
             actual_money = show_money(user_id)
             print("Your money is: {}".format(actual_money))
@@ -25,24 +29,27 @@ def atm(username, user_id):
         elif option == 5:
             print("\nbye\n")
             exit()
-        else:
-            print("I dont know that option!")
-            exit()
     except KeyboardInterrupt:
         print("\nbye\n")
+    #waiting for errors
     except Exception as e:
         print(e)
 
+#This function adds money to the user's account statement
 def deposit_money(username, user_id):
+    #connecting with the database sqlite
     with sqlite3.connect('data.db') as connection:
+        #create a cursor
         cursor = connection.cursor()
         try:
+            #asking for the amount to add
             input_money = int(input("How much money do you want to deposit: "))
-
+            #get to the actual amount of money in the account statement; using the show_money function
             actual_money = show_money(user_id)
-
+            #then add the acual money and the input money to upload to the database
             input_money = input_money + actual_money
 
+            #upload money using the input money and the username
             sql_sentence = "UPDATE users SET money = (?) where username = (?)"
             data = (input_money, username)
 
@@ -51,52 +58,60 @@ def deposit_money(username, user_id):
 
             cursor.execute(sql_sentence, data)
             connection.commit()
-            #cursor.close()
             print("Transaction has bee completed successfully!")
-            #connection.close()
+        #waiting for errors
         except Exception as e:
             print(e)
         except KeyboardInterrupt:
             print("\nbye\n")
+        #waiting for errors from the database
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
 
+#This function remains money from the database
 def withdraw_money(user_id):
+    #connecting with the database sqlite
     with sqlite3.connect('data.db') as connection:
+        #create a cursor
         cursor = connection.cursor()
-        user_id = int(user_id[0])
         try:
+            #asking to user how much money want to reamins
             output_money = int(input("How much money do you wish to withdraw?: "))
+            #get to the actual amount of money in the account statement; using the show_money function
             actual_money = show_money(user_id)
 
             print("Prosessing...")
             time.sleep(3)
+            #just checking that the user has enough money in the database.
             if actual_money < output_money:
                 print("You dont have enough money!")
                 exit()
             else:
 
-                #print("some")
+                #checking that the user has enough money in the database
                 actual_money = actual_money - output_money
 
-                #print(actual_money[0])
+                #and then upload the data
                 sql_sentence = "UPDATE users SET money = (?)  where id = (?)"
                 data = (actual_money, user_id)
-
                 cursor.execute(sql_sentence, data)
                 connection.commit()
                 print("Your money now is: {}".format(actual_money))
-                #connection.close()
 
+        #waiting for errors
         except Exception as e:
             print(e)
         except KeyboardInterrupt:
             print("\nbye\n")
+        #waiting for errors from the database
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
 
+#This function just receive the user id to return the actual money from that user id
 def show_money(user_id):
+    #connecting with the database sqlite
     with sqlite3.connect('data.db') as connection:
+        #creating a cursor
         cursor = connection.cursor()
         try:
             sql_sentence = "SELECT money from users where id = (?)"
@@ -104,60 +119,92 @@ def show_money(user_id):
             cursor.execute(sql_sentence, data)
             connection.commit()
             actual_money = cursor.fetchone()
+            #actual_money it is received in the form of tuple and then becomes to int
             actual_money = int(actual_money[0])
-            #connection.close()
             return actual_money
+        #waiting for errors
         except Exception as e:
             print(e)
         except KeyboardInterrupt:
             print("\nbye\n")
+        #waiting for errors from the database
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
 
+#This function remains to the user and then add that money to the specified user
 def deposit_to_other_client(user_id, username):
+    #connecting with the database sqlite
     with sqlite3.connect('data.db') as connection:
+        #creating a cursor
         cursor = connection.cursor()
         try:
+            #getting the username to transfer
             username_to = input("What is the username you want to transfer to?: ")
             cursor.execute("SELECT * from users WHERE username = (?)", (username_to,))
-            #connection.commit()
 
+            #check if that user exist
             if cursor.fetchone() is not None:
+                #ask how much money wants to remains
                 money_to = int(input("How much money you want to transfer?: "))
+                #get to the actual amount of money in the account statement; using the show_money function
                 actual_money = show_money(user_id)
 
+                #just checking that the user has enough money in the database.
                 if actual_money < money_to:
                     print("You dont have enough money to realize this operation!")
-                    #connection.close()
                     exit()
+                #if the user have enough money
                 else:
+                    #last warning before transfer
+                    #as if it were real money lol
                     option = input("Are you complete sure to transfer {} to {} y/N: ".format(money_to, username_to))
                     if option.lower() == "y":
                         print("Prosessing...")
                         time.sleep(3)
+                        #getting the actual money from the usere to transfer
                         cursor.execute("SELECT money from users WHERE username = (?)", (username_to,))
                         connection.commit()
                         username_to_actual_money = cursor.fetchone()
+                        #username_to_actual_money it is received in the form of tuple and then becomes to int
                         username_to_actual_money = int(username_to_actual_money[0])
+                        #here remains the actual_money to the money to transfer
                         actual_money = actual_money - money_to
+                        #here add the money to transfer to the user money to transfer
                         money_to = money_to + username_to_actual_money
 
+                        #then updata the database using the new data
                         cursor.execute("UPDATE users SET money = (?) where username = (?)", (money_to, username_to))
                         connection.commit()
                         cursor.execute("UPDATE users SET money = (?) where username = (?)", (actual_money, username))
                         connection.commit()
-                        #connection.close()
                         print("Transaction has bee completed successfully!")
                         print("Your money is now: {}".format(actual_money))
 
+                    elif option.lower() == 'n':
+                        #asking the user if he wants to enter the ATM, very obvious
+                        print("Operation cancelled")
+                        option = input("Do you need to enter into the ATM again? y/N:")
+                        if option.lower() == 'y':
+                            print("login...")
+                            time.splee(3)
+                            atm(username, user_id)
+                        elif option.lower() == 'n':
+                            print("I'll see you later then!")
+                            exit()
+                        else:
+                            print("I dont know that option!")
+                            exit()
                     else:
-                        #connection.close()
+                        print("I dont know that option!")
                         exit()
             else:
                 print("That user do not exist!")
-                #connection.close()
                 exit()
+        #waiting for errors
         except Exception as e:
             print(e)
         except KeyboardInterrupt:
             print("\nbye\n")
+        #waiting for errors from the database
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 import os, getpass, sqlite3, hashlib, time
 from atm import *
+from art import *
 
+#This is the main function, whats print the main menu and ask for the operation to do
 def main():
 
     try:
-        #os.system("clear")
+        os.system("clear")
         #printing the main menu
-        print("""Welcom to the best login system ever created!
+        tprint("Login  system")
+        print("Welcome to the best login system ever created!")
+        print("""
             1 to login account.
             2 to create account.
             3 to delete your account.
-            4 to exit or use CTRL+C""")
+            4 to exit or use CTRL+C
+            """)
+        #asking for the operation to do
         option = int(input("Write your option: "))
         if option == 1:
             login_account()
@@ -27,6 +33,7 @@ def main():
             exit()
     except KeyboardInterrupt:
         print("\nbye\n")
+    #waiting for errors
     except Exception as e:
         print(e)
 
@@ -59,17 +66,16 @@ def delete_account():
                     print("Deleting acount...")
                     time.sleep(3)
 
-                    #delete account
+                    #delete account code
                     sql_sentence = "DELETE from users WHERE username = (?) AND password = (?)"
                     data = (user, crypted_passwd)
                     cursor.execute(sql_sentence, data)
                     connection.commit()
                     print("Acount deleted succefully!")
 
-
                 elif option.lower() == "n":
                     print("Then you are welcome.")
-
+                    #connection.close()
                 else:
                     print("I dont know that option.")
                     print("Try it again")
@@ -98,17 +104,14 @@ def get_user_id(username, password):
             connection.commit()
 
             user_id = cursor.fetchone()
-            # user_id it is received in the form of tuple and then becomes to int
+            #user_id it is received in the form of tuple and then becomes to int
             user_id = int(user_id[0])
             #and then returns it
             return user_id
-        #waiting for errors
         except Exception as e:
             print(e)
-        #waiting for errors from the database
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
-
 #This function just This function check that the user exists and then login into the ATM system
 def login_account():
     #connecting with the database sqlite
@@ -119,7 +122,6 @@ def login_account():
 
         try:
             for attempts in range(MAX_ATTEMPTS):
-
                 #getting for the username and password
                 username = input("Enter the user name: ")
                 passwd = getpass.getpass(prompt = "Password: ", stream = None)
@@ -128,6 +130,7 @@ def login_account():
                 #login code
                 sql_sentence = "SELECT * from users WHERE username = (?) AND password = (?)"
                 data = (username, crypted_passwd)
+
                 cursor.execute(sql_sentence, data)
                 connection.commit()
                 print("login...")
@@ -151,51 +154,64 @@ def login_account():
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
 
+#This function just create account
 def create_account():
-
+    #connecting with the database sqlite
     with sqlite3.connect('data.db') as connection:
+        #create a cursor
         cursor = connection.cursor()
         try:
-
-            user = input("Enter the user name: ")
-            cursor.execute("SELECT * from users WHERE username = (?)", (user,))
+            #getting for the username
+            username = input("Enter the user name: ")
+            cursor.execute("SELECT * from users WHERE username = (?)", (username,))
             connection.commit()
 
+            #ask to the database if that username its already in use
             if cursor.fetchone() is not None:
                 print("This name is already in use...")
-                #cursor.close()
-                #connection.close()
                 exit()
             else:
+                #getting for the password 2 times to check if one of the passwords its wrong
                 passwd = getpass.getpass(prompt = "Password: ", stream = None)
                 retyped_passwd = getpass.getpass(prompt = "Re-type your password: ", stream = None)
                 money = 0
+                #checking the passwords match
                 if retyped_passwd == passwd:
-
+                    #encrypting and encoding password to save en the database
                     crypted_passwd = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
                     sql_sentence = "INSERT INTO users(username, password, money) VALUES(?, ?, ?)"
-                    data = (user, crypted_passwd, money)
+                    data = (username, crypted_passwd, money)
 
                     print("Creating acount...")
 
                     time.sleep(3)
-
-                    #connection = sqlite3.connect('data.db')
-                    #cursor = connection.cursor()
                     cursor.execute(sql_sentence, data)
                     connection.commit()
-                    #cursor.close()
                     print("User created successfully")
-                    #connection.close()
+                    #asking the user if he wants to enter the ATM, very obvious
+                    option = input("Do you need to enter into the ATM? y/N:")
+                    if option.lower() == 'y':
+                        print("login...")
+                        time.sleep(3)
+                        #get to the user id using the get_user_id function
+                        user_id = get_user_id(username, crypted_passwd)
+                        atm(username, user_id)
+                    elif option.lower() == 'n':
+                        print("I'll see you later then!")
+                        exit()
+                    else:
+                        print("I dont know that option!")
+                        exit()
                 else:
                     print("The passwords do not match!")
-                    #cursor.close()
-                    #connection.close()
-                    #exit()
+                    print("Try it again! ")
+                    create_account()
+        #waiting for errors
         except Exception as e:
             print(e)
         except KeyboardInterrupt:
             print("\nbye\n")
+        #waiting for errors from the database
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
 
